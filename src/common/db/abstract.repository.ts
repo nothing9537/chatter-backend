b02fe2b34919ddca681edd6075d632a6fe0f6e4e
Nothingg <nothing9537@gmail.com>
2025-01-16 14:05:53 -0600

    finalize users repository and schemas

--- src/common/db/abstract.repository.ts
+++ src/common/db/abstract.repository.ts
@@ -1,26 +1,24 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
-import { AbstractDocument } from './abstract.schema';
+import { AbstractEntity } from './abstract.entity';
 
-export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+export abstract class AbstractRepository<T extends AbstractEntity> {
   protected abstract readonly logger: Logger;
 
-  constructor(protected readonly model: Model<TDocument>) {}
+  constructor(protected readonly model: Model<T>) {}
 
-  public async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  public async create(document: Omit<T, '_id'>): Promise<T> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
     });
 
-    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+    return (await createdDocument.save()).toJSON() as unknown as T;
   }
 
-  public async findOne(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+  public async findOne(filterQuery: FilterQuery<T>): Promise<T> {
+    const document = await this.model.findOne(filterQuery).lean<T>();
 
     if (!document) {
       this.logger.warn("Document wasn't found with filterQuery", filterQuery);
@@ -32,14 +30,14 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   public async findOneAndUpdate(
-    filterQuery: FilterQuery<TDocument>,
-    update: UpdateQuery<TDocument>,
-  ): Promise<TDocument> {
+    filterQuery: FilterQuery<T>,
+    update: UpdateQuery<T>,
+  ): Promise<T> {
     const document = await this.model
       .findOneAndUpdate(filterQuery, update, {
         new: true,
       })
-      .lean<TDocument>();
+      .lean<T>();
 
     if (!document) {
       this.logger.warn("Document wasn't found with filterQuery", filterQuery);
@@ -50,13 +48,11 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     return document;
   }
 
-  public async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
-    return this.model.find(filterQuery).lean<TDocument[]>();
+  public async find(filterQuery: FilterQuery<T>): Promise<T[]> {
+    return this.model.find(filterQuery).lean<T[]>();
   }
 
-  public async findOneAndDelete(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  public async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
+    return this.model.findOneAndDelete(filterQuery).lean<T>();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
   create(createUserInput: CreateUserInput) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  public async findAll() {
+    return this.usersRepository.find({});
   }
 
   findOne(id: number) {
