3c145453699d623d49c025d0ef8c87ad74266e92
Nothingg <nothing9537@gmail.com>
2025-01-17 13:49:26 -0600

    add secure CRUD operations

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,9 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/token-payload.interface';
 
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
@@ -15,22 +20,29 @@ export class UsersResolver {
   }
 
   @Query(() => [User], { name: 'users' })
+  @UseGuards(GqlAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
+  @UseGuards(GqlAuthGuard)
   findOne(@Args('_id') _id: string) {
     return this.usersService.findOne(_id);
   }
 
   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput._id, updateUserInput);
+  @UseGuards(GqlAuthGuard)
+  updateUser(
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.usersService.update(user._id, updateUserInput);
   }
 
   @Mutation(() => User)
-  removeUser(@Args('_id') _id: string) {
-    return this.usersService.remove(_id);
+  @UseGuards(GqlAuthGuard)
+  removeUser(@CurrentUser() user: TokenPayload) {
+    return this.usersService.remove(user._id);
   }
 }
