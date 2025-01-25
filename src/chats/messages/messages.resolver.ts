1a171777edb8b01dfc2b619e93caa86edd5369b7
Nothingg <nothing9537@gmail.com>
2025-01-25 14:16:46 -0600

    get messages

--- src/chats/messages/messages.resolver.ts
+++ src/chats/messages/messages.resolver.ts
@@ -1,11 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { MessagesService } from './messages.service';
-import { Message } from './entities/message.entity';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
+
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
-import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
+
+import { CreateMessageInput } from './dto/create-message.input';
+import { MessagesService } from './messages.service';
+import { Message } from './entities/message.entity';
+import { GetMessagesArgs } from './dto/get-messages.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
@@ -18,5 +21,14 @@ export class MessagesResolver {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.messagesService.createMessage(createMessageInput, user._id);
+  }
+
+  @Query(() => [Message], { name: 'messages' })
+  @UseGuards(GqlAuthGuard)
+  public async getMessages(
+    @Args() getMessagesArgs: GetMessagesArgs,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.messagesService.getMessages(getMessagesArgs, user._id);
   }
 }
--- src/chats/messages/messages.service.ts
+++ src/chats/messages/messages.service.ts
@@ -4,10 +4,11 @@ import { Types } from 'mongoose';
 import { CreateMessageInput } from './dto/create-message.input';
 import { ChatsRepository } from '../chats.repository';
 import { Message } from './entities/message.entity';
+import { GetMessagesArgs } from './dto/get-messages.args';
 
 @Injectable()
 export class MessagesService {
-  constructor(private readonly chatsRepository: ChatsRepository) {}
+  constructor(private readonly chatsRepository: ChatsRepository) { }
 
   public async createMessage(
     { content, chatId }: CreateMessageInput,
@@ -23,14 +24,7 @@ export class MessagesService {
     await this.chatsRepository.findOneAndUpdate(
       {
         _id: chatId,
-        $or: [
-          { userId },
-          {
-            userIds: {
-              $in: [userId],
-            },
-          },
-        ],
+        ...this.userChatFilter(userId),
       },
       {
         $push: {
@@ -40,5 +34,27 @@ export class MessagesService {
     );
 
     return message;
+  }
+
+  public async getMessages({ chatId }: GetMessagesArgs, userId: string) {
+    return (
+      await this.chatsRepository.findOne({
+        _id: chatId,
+        ...this.userChatFilter(userId),
+      })
+    ).messages;
+  }
+
+  private userChatFilter(userId: string) {
+    return {
+      $or: [
+        { userId },
+        {
+          userIds: {
+            $in: [userId],
+          },
+        },
+      ],
+    };
   }
 }
